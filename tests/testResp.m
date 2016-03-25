cd57abc9b7df46cbd6c3cf0bae76932a0e32558f
Alex Forrence <aforren1@jhu.edu>
2016-03-25 08:32:12 -0400

    Update test

--- tests/testResp.m
+++ tests/testResp.m
@@ -5,7 +5,7 @@ try
     addpath src
     CCCOMBO = 5;
     [resp_consts, scrn_consts, misc_consts] = mkConstants;
-    tgt = mapRapid(dlmread(['misc/tfiles/','dy3_bk4_ez0_sw1_sh1.tgt']));
+    tgt = mapRapid(dlmread(['misc/tfiles/','testBehav_rapid.tgt']));
     fingers = unique(tgt.finger);
     dev = mkRespDev(kybrd, fingers, resp_consts);
     if strcmpi(dev.type, 'keyboard')
