3cf43f68de2f60f6b22ef6e518511389ac0971a8
Alex Forrence <aforren1@jhu.edu>
2016-03-27 21:33:01 -0400

    Initial exploration

---
+++ analysis/import_and_clean.R
@@ -0,0 +1,55 @@
+
+importRapid <- function() {
+  base_dir <- '/home/aforrence/Documents/BLAM/finger-4/data'
+  all_files <- list.files(path = base_dir, full.names = TRUE)
+  tr <- list.files(path = base_dir, pattern = 'ez', full.names = TRUE)
+  rapid <- all_files[!all_files %in% tr] 
+  ii <- 1
+  for (file in rapid) {
+    if (ii == 1) {
+      final_dat <- read.table(file, header = FALSE, sep = ',')
+    } else {
+        temp_dat <- read.table(file, header = FALSE, sep = ',')
+        final_dat <- rbind(final_dat, temp_dat)
+        rm(temp_dat)
+    }
+    ii = ii + 1
+  }
+  names(final_dat) <- c('id', 'day', 'block', 'trial', 
+                        'swapped', 'img_type', 'finger',
+                        'swap1', 'swap2', 'resp1', 't_resp1',
+                        'resp2', 't_resp2', 'resp3', 't_resp3')
+  
+  final_dat[final_dat == -1] <- NA
+  final_dat$correct <- final_dat$resp1 == final_dat$finger
+  
+
+  final_dat
+}
+
+
+importTR <- function() {
+  base_dir <- '/home/aforrence/Documents/BLAM/finger-4/data'
+  tr <- list.files(path = base_dir, pattern = 'ez', full.names = TRUE)
+  ii <- 1
+  for (file in tr) {
+    if (ii == 1) {
+      final_dat <- read.table(file, header = FALSE, sep = ',')
+    } else {
+      temp_dat <- read.table(file, header = FALSE, sep = ',')
+      final_dat <- rbind(final_dat, temp_dat)
+      rm(temp_dat)
+    }
+    ii = ii + 1
+  }
+  names(final_dat) <- c('id', 'day', 'block', 'trial', 'easy',
+                        'swapped', 'img_type', 'finger', 't_img',
+                        'swap1', 'swap2', 'resp1', 't_resp1', 
+                        'resp2', 't_resp2', 'resp3', 't_resp3')
+  
+  final_dat[final_dat == -1] <- NA
+  final_dat$correct <- final_dat$resp1 == final_dat$finger
+  final_dat$t_prep <- final_dat$t_resp1 - final_dat$t_img
+  final_dat
+  
+}
